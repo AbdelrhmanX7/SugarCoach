ec8d4600a084fc6577a1e4665d3011e8632d22a7
AbdelrhmanX7 <hadh2819@gmail.com>
2026-03-23 00:35:55 -0700

    feat(bluetooth): add Web Bluetooth API type declarations

---
+++ types/web-bluetooth.d.ts
@@ -0,0 +1,60 @@
+// Web Bluetooth API type declarations
+// https://webbluetoothcg.github.io/web-bluetooth/
+
+interface BluetoothRequestDeviceFilter {
+  services?: BluetoothServiceUUID[];
+  name?: string;
+  namePrefix?: string;
+}
+
+interface RequestDeviceOptions {
+  filters?: BluetoothRequestDeviceFilter[];
+  optionalServices?: BluetoothServiceUUID[];
+  acceptAllDevices?: boolean;
+}
+
+interface BluetoothRemoteGATTServer {
+  device: BluetoothDevice;
+  connected: boolean;
+  connect(): Promise<BluetoothRemoteGATTServer>;
+  disconnect(): void;
+  getPrimaryService(
+    service: BluetoothServiceUUID,
+  ): Promise<BluetoothRemoteGATTService>;
+}
+
+interface BluetoothRemoteGATTService {
+  device: BluetoothDevice;
+  uuid: string;
+  getCharacteristic(
+    characteristic: BluetoothCharacteristicUUID,
+  ): Promise<BluetoothRemoteGATTCharacteristic>;
+}
+
+interface BluetoothRemoteGATTCharacteristic extends EventTarget {
+  service: BluetoothRemoteGATTService;
+  uuid: string;
+  value: DataView | null;
+  startNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  stopNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  readValue(): Promise<DataView>;
+  writeValue(value: BufferSource): Promise<void>;
+}
+
+interface BluetoothDevice extends EventTarget {
+  id: string;
+  name: string | null;
+  gatt: BluetoothRemoteGATTServer | null;
+}
+
+interface Bluetooth extends EventTarget {
+  requestDevice(options: RequestDeviceOptions): Promise<BluetoothDevice>;
+  getAvailability(): Promise<boolean>;
+}
+
+type BluetoothServiceUUID = number | string;
+type BluetoothCharacteristicUUID = number | string;
+
+interface Navigator {
+  bluetooth: Bluetooth;
+}
